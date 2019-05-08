94580966c47c13cf9e7e47e1c3965cb0f47d6272
Evaldo Cardoso <Evaldopkscardoso@gmail.com>
2019-05-07 22:26:50 -0300

    tela de inicio
    criação da tela de inicio

--- pages/inicio.php
+++ pages/inicio.php
@@ -8,10 +8,22 @@
         <div class="conteiner-inicio">
             <div class="filtro">
                 <div class="bkg-inicio">
-                    <div class="box-inicio p-l-50 p-r-50 p-t-77 p-b-30">
-                        <div class="menu-inicio"></div>
-                        <div class="menu-inicio"></div>
-                        <div class="menu-inicio"></div>
+                    <div class="logo-remind" style="color:white;">
+                        Re<span style="color: #01aef0;" >Mind</span>
+                    </div>
+                    <div class="box-inicio p-l-50 p-r-50 p-b-30">
+                        <div class="menu-inicio">
+                            <i class="fas fa-user-md icone-inicio"></i>
+                            <span class="categoria">
+                                <a class="categoria" href="login.php"> Terapeuta</a>
+                            </span>
+                        </div>
+                        <div class="menu-inicio">
+                            <i class="fas fa-heartbeat icone-inicio"></i>
+                            <span class="categoria">
+                                <a class="categoria" href="login.php"> Paciente</a>
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -19,4 +31,4 @@
 
         
     </body>
-</html>
+</html> 
