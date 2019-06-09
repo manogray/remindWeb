284aecf69534eed46b5b2dfc2e23e18bc80f7164
Evaldo Cardoso <Evaldopkscardoso@gmail.com>
2019-06-09 16:06:08 -0300

    Cadastro Terapeuta UI fix

--- cadastroTerapeuta.php
+++ cadastroTerapeuta.php
@@ -25,7 +25,7 @@
 
                         <div class="form-input">
                             <label for="telefone" class="required">CPF</label>
-                            <input type="text" required name="cpf" id="telefone">
+                            <input type="text" required name="cpf" id="cpf">
                         </div>
 
                         <div class="form-input">
@@ -35,64 +35,21 @@
                             <input class="radio-area" type="radio" required name="tRegistro" value="false"/>Matrícula
                         </div>
 
-                        <div class="form-input">
+                        <div class="form-input" id="regisT">
                             <label class="required">Registro</label>
                             <input type="text" name="registro">
                         </div>
 
-                        <div class="form-input">
+
+                        <div class="form-input" id="emailT">
                             <label for="email" class="required">Email</label>
                             <input style="width: 400px;" type="email" required name="email" id="email">
                         </div>
-                        <div class="form-input">
+                        <div class="form-input" id="tellT">
                             <label for="telefone" class="required">Telefone</label>
-                            <input type="text" required name="telefone" id="telefone">
+                            <input type="text" required name="telefone" id="telefoneT">
                         </div>
-                                                
-                        <div class="quadroHorarios">
-                            <label class="required" style="width: 100%;">Disponibilidade</label>
-                            <div class="dia">
-                                <span>Segunda-feira</span>
-                                <div style="display: flex;">
-                                    <input type="time" name="timeSegIni">
-                                    <span style="margin: 0px 6px;">às</span>
-                                    <input type="time" name="timeSegFim">
-                                </div>
-                            </div>
-                            <div class="dia">
-                                <span>Terça-feira</span>
-                                <div style="display: flex;">
-                                    <input type="time" name="timeTerIni">
-                                    <span style="margin: 0px 6px;">às</span>
-                                    <input type="time" name="timeTerFim">
-                                </div>
-                            </div>
-                            <div class="dia">
-                                <span>Quarta-feira</span>
-                                <div style="display: flex;">
-                                    <input type="time" name="timeQuaIni">
-                                    <span style="margin: 0px 6px;">às</span>
-                                    <input type="time" name="timeQuaFim">
-                                </div>
-                            </div>
-                            <div class="dia">
-                                <span>Quinta-feira</span>
-                                <div style="display: flex;">
-                                    <input type="time" name="timeQuiIni">
-                                    <span style="margin: 0px 6px;">às</span>
-                                    <input type="time" name="timeQuiFim">
-                                </div>
-                            </div>
-                            <div class="dia">
-                                <span>Sexta-feira</span>
-                                <div style="display: flex;">
-                                    <input type="time" name="timeSexIni">
-                                    <span style="margin: 0px 6px;">às</span>
-                                    <input type="time" name="timeSexFim">
-                                </div>
-                            </div>
-                        </div>
-                        
+
                         <div class="form-input">
                             <label for="senha" class="required">Senha</label>
                             <input type="password" required name="senha" id="senha">
@@ -102,6 +59,54 @@
                             <label for="confirmar" class="required">Confirmar Senha</label>
                             <input type="password" required name="confimar" id="confirmar">
                         </div>
+                                                
+                        <div class="quadroHorarios">
+                            <div class="barato-demais">
+                                <label class="required" style="width: 100%;">Disponibilidade</label>
+                                <div class="dia">
+                                    <span class="semana">Segunda-feira</span>
+                                    <div class="hora" style="display: flex;">
+                                        <input class="time-input" type="time" name="timeSegIni">
+                                        <span class="semana" style="margin: 0px 6px;">às</span>
+                                        <input class="time-input" type="time" name="timeSegFim">
+                                    </div>
+                                </div>
+                                <div class="dia">
+                                    <span class="semana">Terça-feira</span>
+                                    <div class="hora" style="display: flex;">
+                                        <input class="time-input" type="time" name="timeTerIni">
+                                        <span class="semana" style="margin: 0px 6px;">às</span>
+                                        <input class="time-input" type="time" name="timeTerFim">
+                                    </div>
+                                </div>
+                                <div class="dia">
+                                    <span class="semana">Quarta-feira</span>
+                                    <div class="hora" style="display: flex;">
+                                        <input class="time-input" type="time" name="timeQuaIni">
+                                        <span class="semana" style="margin: 0px 6px;">às</span>
+                                        <input class="time-input" type="time" name="timeQuaFim">
+                                    </div>
+                                </div>
+                                <div class="dia">
+                                    <span class="semana">Quinta-feira</span>
+                                    <div class="hora" style="display: flex;">
+                                        <input class="time-input" type="time" name="timeQuiIni">
+                                        <span class="semana" style="margin: 0px 6px;">às</span>
+                                        <input class="time-input" type="time" name="timeQuiFim">
+                                    </div>
+                                </div>
+                                <div class="dia">
+                                    <span class="semana">Sexta-feira</span>
+                                    <div class="hora" style="display: flex;">
+                                        <input class="time-input" type="time" name="timeSexIni">
+                                        <span class="semana" style="margin: 0px 6px;">às</span>
+                                        <input class="time-input" type="time" name="timeSexFim">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
                 </div>
                 <div class="form-submit">
                     <input style="margin-bottom: 25px;" type="submit" value="Salvar cadastro" class="submit" id="submit" name="submit">
